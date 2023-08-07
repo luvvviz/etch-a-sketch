324ec561abb4585ed0fbc5d08d2de4a7aa1db275
luvvviz <139688268+luvvviz@users.noreply.github.com>
2023-08-07 21:20:29 +0200

    Added random color. Fixed left behind squares.
    container width / num of rows => gridTemplateColumns/Rows

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const container = document.querySelector('#container');
 const btn = document.querySelector('button');
 btn.addEventListener("click", getPlayerChoice);
+const btnBlack = document.querySelector('.black');
+const btnColor = document.querySelector('.color');
+createGrid(16, 16);
+
+let activeButton = null;
+
+btnBlack.addEventListener('click', () => {
+    toggleButtonState(btnBlack);
+});
+btnColor.addEventListener('click', () => {
+    toggleButtonState(btnColor);
+});
+
+function toggleButtonState(button) {
+    if (button === activeButton) {
+        activeButton.classList.remove('pressed');
+        activeButton = null;
+    } else {
+        if (activeButton) {
+            activeButton.classList.remove('pressed');
+        }
+        activeButton = button;
+        activeButton.classList.add('pressed');
+    }
+    return activeButton;
+}
 
 function getPlayerChoice() {
     let playerChoice = prompt('What is your dimensions of choice?', '');
@@ -13,18 +39,29 @@ function getPlayerChoice() {
 
 function createGrid(rows, columns) {
     container.replaceChildren();
+    container.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
     for (let i = 0; i < rows * columns; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
-        const squareWidth = Math.floor(640 / rows);
-        square.setAttribute('style', `width: ${squareWidth}px`);
-
         container.appendChild(square);
-
-        square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = 'black';
-        });
-
         
+        square.addEventListener("mouseover", () => {
+            if (btnBlack === activeButton) {
+                square.style.backgroundColor = 'black';
+            } else if (btnColor === activeButton) {
+                const randomColor = getRandomColor();
+                square.style.backgroundColor = randomColor;
+            }
+        });
     }
+}
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
